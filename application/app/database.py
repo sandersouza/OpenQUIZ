310759e78844678b00a1bf7ac3588565b72dfb82
sandersouza <sander@unixwork.com.br>
2024-12-12 11:09:07 -0300

    Pull database params from .env on root of stack

--- application/app/database.py
+++ application/app/database.py
@@ -1,8 +1,18 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# Caminho para o arquivo .env localizado em ../../.env
+env_path = Path(__file__).resolve().parents[2] / '.env'
+load_dotenv(dotenv_path=env_path)
 
 async def connect_to_mongo(app):
     try:
-        client = AsyncIOMotorClient("mongodb://mongo:27017")
+        mongo_uri = os.getenv("MONGO_URI")
+        if not mongo_uri:
+            raise ValueError("A variável de ambiente MONGO_URI não está definida.")
+        client = AsyncIOMotorClient(mongo_uri)
         app.state.db = client["openquiz"]
         print("Conexão com MongoDB estabelecida.")
     except Exception as e:
